3e0c89701d2388a1e4be5849f852af4ee8080feb
Simon Lehner-Dittenberger <simon.lehner-dittenberger@cloudflight.io>
2022-07-30 16:42:48 +0200

    Add package.json and watch config

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-var http = require('http'); // 1 - Import Node.js core module
+var http = require('http');
 
-var serve = serveStatic
+var finalhandler = require('finalhandler');
+var serveStatic = require('serve-static');
 
+var serve = serveStatic("./src/");
 
-var server = http.createServer(function (req, res) {   // 2 - creating server
-
-  
-
+var server = http.createServer(function(req, res) {
+    var done = finalhandler(req, res);
+    serve(req, res, done);
 });
-
 server.listen(5000); //3 - listen for any incoming requests
 
 console.log('Node.js web server at port 5000 is running..')
