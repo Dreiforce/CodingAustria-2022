67fcf663f168b484a9747ae2f9d8dcbbabd5f83e
Simon Lehner-Dittenberger <simon.lehner-dittenberger@cloudflight.io>
2022-07-30 17:13:32 +0200

    Add express and serve static files and api

--- server.js
+++ server.js
@@ -1,14 +1,47 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
-var finalhandler = require('finalhandler');
-var serveStatic = require('serve-static');
 
-var serve = serveStatic("./src/");
+var tasklist = [{
+    "id": "1",
+    "name": "Water the tree",
+    "description": "",
+    steps: [
+        "Take watering can",
+        "Fill with water",
+        "Water the tree"
+    ]
+},{
+    "id": "2",
+    "name": "Water the tree",
+    "description": "",
+    steps: [
+        "Take watering can",
+        "Fill with water",
+        "Water the tree"
+    ]
+}]
 
-var server = http.createServer(function(req, res) {
-    var done = finalhandler(req, res);
-    serve(req, res, done);
-});
-server.listen(5000); //3 - listen for any incoming requests
+console.log(__dirname)
+//setting middleware
+app.use(express.static(__dirname + '/src')); //Serves resources from src
+app.use(express.static(__dirname + '/node_modules/leaflet/dist'))
 
+const api = express.Router()
+api.get('/task/', (req, res, next) => {
+    // render a regular page
+    res.send(tasklist)
+})
+api.get('/task/:id', (req, res, next) => {
+    // render a regular page
+    res.send(tasklist.filter(s => {
+        console.log(s.id + " " + req.params.id)
+        return s.id === req.params.id
+    }))
+})
+
+app.use('/api', api)
+
+
+var server = app.listen(5000);
 console.log('Node.js web server at port 5000 is running..')
